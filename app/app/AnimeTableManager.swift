f34f710762555a8edd2086f04656c4a4cec18672
SolarHaddock46 <vladimir@matsnev.ru>
2023-11-20 15:31:40 +0300

    added AnimeTableManager

---
+++ app/app/AnimeTableManager.swift
@@ -0,0 +1,29 @@
+//
+//  AnimeTableManager.swift
+//  app
+//
+//  Created by Владимир Мацнев on 20.11.2023.
+//
+
+import Foundation
+import UIKit
+
+final class AnimeTableManager: NSObject {
+    var animeData: [String: Anime] = [:]
+}
+
+extension AnimeTableManager: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animeData.values.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let anime = Array(animeData.values)[indexPath.row]
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "\(anime.title) / \(anime.titleJapanese)\nMAL ID: \(anime.malId) / Genre: \(anime.genres?.first?.name ?? "N/A")"
+        configuration.secondaryText = anime.synopsis
+        cell.contentConfiguration = configuration
+        return cell
+    }
+}
